a0a3ef1b561c6bc05dc5145472bb1d999c50b86e
rosemqa <ers6wobmo09m@mail.ru>
2024-08-15 01:26:24 +0300

    renamed fixture in conftest.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from services.auth.api_auth import AuthAPI
 
 
 @pytest.fixture(scope='class')
-def login(request):
+def log_in(request):
     """Login and get auth cookie"""
     auth = AuthAPI()
     request.cls.auth_cookie = auth.login_user()['auth_cookie']
